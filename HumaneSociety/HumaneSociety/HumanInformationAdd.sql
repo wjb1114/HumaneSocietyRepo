8448e36baee0e37591802e0b00e123889c4552e6
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-30 15:37:20 -0500

    Adding the initial insertion template of what was added to local database to perform CRUD operations later in the project

---
+++ HumaneSociety/HumaneSociety/HumanInformationAdd.sql
@@ -0,0 +1,57 @@
+
+INSERT INTO Categories VALUES('Sick and Looking');
+INSERT INTO Categories VALUES('Healthy and Looking');
+INSERT INTO Categories VALUES('Healthy and Adopted');
+INSERT INTO Categories VALUES('Newly Sheltered');
+INSERT INTO Categories VALUES('Elderly');
+
+INSERT INTO Animals VALUES ('Eduardo', 120, 6,'Friendly and Loveable',0,1,'Female','Ready To Adopt', 4, 3, 4)
+INSERT INTO Animals VALUES ('Jackson', 10, 2,'Guarding',1,1,'Female','Needs Vaccinations', 2, 1, 4)
+INSERT INTO Animals VALUES ('Percy', 63, 7,'Young and Energetic',0,0,'Male','Ready To Adopt', 5, 2, 2)
+INSERT INTO Animals VALUES ('Copper', 29, 3,'Loud and Controlling',1,1,'Male','Ready To Adopt', 1, 2, 1)
+INSERT INTO Animals VALUES ('Mayvis', 18, 1,'Anxious and Skiddish',0,1,'Female','Needs Neutering', 1, 3, 4)
+
+INSERT INTO DietPlans VALUES('Vegetarian','Pure Vegetables', 5);
+INSERT INTO DietPlans VALUES('Carnivore','Pure Meat', 2);
+INSERT INTO DietPlans VALUES('Pescatarian','Pure Fish', 4);
+INSERT INTO DietPlans VALUES('Keto','No carbohydrates', 3);
+INSERT INTO DietPlans VALUES('Vegan','All plant based produce', 6);
+
+INSERT INTO Employees VALUES('John','Smith','JSmith01', 'HeresJohnny');
+INSERT INTO Employees VALUES('Martha','May','MarthaMay', 'MayistheWay');
+INSERT INTO Employees VALUES('Tim','Kook','TKAllDay', 'CantStopTKNoWay');
+INSERT INTO Employees VALUES('Sarah','Johnson','SJohnson', 'SJ212121');
+INSERT INTO Employees VALUES('Mike','Fiers','MikeF', 'HikewithMike');
+
+INSERT INTO Rooms VALUES(1,1);
+INSERT INTO Rooms VALUES(2,1);
+INSERT INTO Rooms VALUES(3,1);
+INSERT INTO Rooms VALUES(4,1);
+INSERT INTO Rooms VALUES(5,1);
+INSERT INTO Rooms VALUES(1,2);
+INSERT INTO Rooms VALUES(2,3);
+INSERT INTO Rooms VALUES(3,4);
+INSERT INTO Rooms VALUES(4,5);
+INSERT INTO Rooms VALUES(1,3);
+
+INSERT INTO Clients VALUES('Toby','McGuire','TMAC','12345', 1, 'TOBYMACATTACK@gmail.com');
+INSERT INTO Clients VALUES('Trisha','McGuire','TRISHMAC','23456', 2, 'TMACATTACK@gmail.com');
+INSERT INTO Clients VALUES('Danny','Frawley','Dfrawl1234','BigGuy', 3, 'DFRAWL@gmail.com');
+INSERT INTO Clients VALUES('Medusa','McDire','TMedusDonalds','72342', 4, 'ManyBraids@gmail.com');
+INSERT INTO Clients VALUES('Salina','Severson','SSquared','RainbowFish', 5, 'SallySev@gmail.com');
+
+INSERT INTO Addresses VALUES ('1234 Wallaby Way','Muskego',1, 53150);
+INSERT INTO Addresses VALUES ('1818 Donald Duck Avenue','Disney',2, 31316);
+INSERT INTO Addresses VALUES ('1969 GoToTheMoon Road','Houston',3, 21334);
+INSERT INTO Addresses VALUES ('1688 N Franklin Place','Milwaukee',4, 53202);
+INSERT INTO Addresses VALUES ('1889 Lombardi Avenue','Green Bay',5, 51001);
+
+
+
+
+
+
+
+
+
+
